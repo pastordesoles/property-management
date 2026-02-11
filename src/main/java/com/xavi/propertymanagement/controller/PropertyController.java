f0d8bb78a9c60090df1178097437915688e63c9c
Xavier Sans <xavi.sans.b@gmail.com>
2026-02-11 20:01:21 +0100

    Refactored endpoints

--- src/main/java/com/xavi/propertymanagement/controller/PropertyController.java
+++ src/main/java/com/xavi/propertymanagement/controller/PropertyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/properties")
+@RequestMapping("/api/v1")
 public class PropertyController {
     @Autowired
     private PropertyService propertyService;
@@ -20,7 +20,7 @@ public class PropertyController {
         return "Hello";
     }
 
-    @PostMapping("/save")
+    @PostMapping("/properties")
     public ResponseEntity<PropertyDTO> saveProperty(@RequestBody PropertyDTO propertyDTO) {
         propertyDTO = propertyService.saveProperty(propertyDTO);
         ResponseEntity<PropertyDTO> responseEntity = new ResponseEntity<>(propertyDTO, HttpStatus.CREATED);
@@ -28,7 +28,7 @@ public class PropertyController {
         return responseEntity;
     }
 
-    @GetMapping("/retrieveAll")
+    @GetMapping("/properties")
     public ResponseEntity<List<PropertyDTO>> getAllProperties() {
         List<PropertyDTO> propertyList = propertyService.getAllProperties();
         ResponseEntity<List<PropertyDTO>> responseEntity = new ResponseEntity<>(propertyList, HttpStatus.OK);
@@ -36,7 +36,7 @@ public class PropertyController {
         return responseEntity;
     }
 
-    @PutMapping("updateProperty/{propertyId}")
+    @PutMapping("/properties/{propertyId}")
     public ResponseEntity<PropertyDTO> updateProperty(@RequestBody PropertyDTO propertyDTO, @PathVariable Long propertyId) {
         propertyDTO = propertyService.updateProperty(propertyDTO, propertyId);
         ResponseEntity<PropertyDTO> responseEntity = new ResponseEntity<>(propertyDTO, HttpStatus.OK);
@@ -44,7 +44,7 @@ public class PropertyController {
         return responseEntity;
     }
 
-    @PatchMapping("/patchProperty/{propertyId}")
+    @PatchMapping("/properties/{propertyId}")
     public ResponseEntity<PropertyDTO> patchProperty(@RequestBody PropertyDTO propertyDTO, @PathVariable Long propertyId) {
         propertyDTO = propertyService.patchProperty(propertyDTO, propertyId);
         ResponseEntity<PropertyDTO> responseEntity = new ResponseEntity<>(propertyDTO, HttpStatus.OK);
@@ -52,7 +52,7 @@ public class PropertyController {
         return responseEntity;
     }
 
-    @DeleteMapping("delete/{propertyId}")
+    @DeleteMapping("/properties/{propertyId}")
     public ResponseEntity<Void> deleteProperty(@PathVariable Long propertyId) {
         propertyService.deleteProperty(propertyId);
 
