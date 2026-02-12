d3bab7ae4550019318e873b16243fe87857c9be3
Xavier Sans <xavi.sans.b@gmail.com>
2026-02-12 18:00:12 +0100

    Field errors

--- src/main/java/com/xavi/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/xavi/propertymanagement/exception/CustomExceptionHandler.java
@@ -2,13 +2,33 @@ package com.xavi.propertymanagement.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException methodArgumentNotValidException) {
+        List<ErrorModel> errors = new ArrayList<>();
+        List<FieldError> fieldErrorList = methodArgumentNotValidException.getBindingResult().getFieldErrors();
+
+        for (FieldError fieldError : fieldErrorList) {
+            ErrorModel errorModel = new ErrorModel();
+            errorModel.setCode(fieldError.getField());
+            errorModel.setMessage(fieldError.getDefaultMessage());
+            errors.add(errorModel);
+        }
+
+        return new ResponseEntity<List<ErrorModel>>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handlerBusinessException(BusinessException businessException) {
         System.out.println(businessException.getMessage());
--- src/main/java/com/xavi/propertymanagement/model/UserDTO.java
+++ src/main/java/com/xavi/propertymanagement/model/UserDTO.java
@@ -2,6 +2,9 @@ package com.xavi.propertymanagement.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,7 +15,11 @@ import lombok.Setter;
 public class UserDTO {
     private Long userId;
     private String ownerName;
+    @NotNull(message = "Owner email is mandatory")
+    @Size(min = 1, max = 50, message = "Owner email should be between 1 to 50 chars")
     private String ownerEmail;
     private String phone;
+    @NotNull(message = "Owner email is mandatory")
+    @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
