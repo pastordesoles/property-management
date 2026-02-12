9cd66b38ae4b1e9cfaa11cf2d6bf1e9286c90cd9
Xavier Sans <xavi.sans.b@gmail.com>
2026-02-12 18:46:49 +0100

    Implemented logging

--- src/main/java/com/xavi/propertymanagement/exception/CustomExceptionHandler.java
+++ src/main/java/com/xavi/propertymanagement/exception/CustomExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.xavi.propertymanagement.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
@@ -13,12 +15,16 @@ import java.util.List;
 @ControllerAdvice
 public class CustomExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
+
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<ErrorModel>> handleFieldValidation(MethodArgumentNotValidException methodArgumentNotValidException) {
         List<ErrorModel> errors = new ArrayList<>();
         List<FieldError> fieldErrorList = methodArgumentNotValidException.getBindingResult().getFieldErrors();
 
         for (FieldError fieldError : fieldErrorList) {
+            logger.info("fieldError: {}", fieldError.getDefaultMessage());
+
             ErrorModel errorModel = new ErrorModel();
             errorModel.setCode(fieldError.getField());
             errorModel.setMessage(fieldError.getDefaultMessage());
@@ -31,7 +37,12 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>> handlerBusinessException(BusinessException businessException) {
-        System.out.println(businessException.getMessage());
+        for (ErrorModel em : businessException.getErrors()) {
+            logger.debug("BusinessException is thrown - level- debug: {} - {}", em.getCode(), em.getMessage());
+            logger.info("BusinessException is thrown - level- info: {} - {}", em.getCode(), em.getMessage());
+            logger.warn("BusinessException is thrown - level-warn: {} - {}", em.getCode(), em.getMessage());
+            logger.error("BusinessException is thrown - level-error: {} - {}", em.getCode(), em.getMessage());
+        }
 
         return new ResponseEntity<List<ErrorModel>>(businessException.getErrors(), HttpStatus.BAD_REQUEST);
     }
